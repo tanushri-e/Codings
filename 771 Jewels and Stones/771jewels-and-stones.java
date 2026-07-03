b0c313e3133942e7e86944b0dc3f983fba456f75
Tanushri <tanumini456@gmail.com>
2026-07-03 06:48:32 +0000

    Code challenge submission for question: 771 Jewels and Stones

---
+++ 771 Jewels and Stones/771jewels-and-stones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) 
+    {
+        int count=0;
+
+        for(char ch:stones.toCharArray())
+        {  
+            if(jewels.indexOf(ch)!=-1)
+            {
+                count++;
+            }
+            
+        }
+        return count;
+    }
+}
