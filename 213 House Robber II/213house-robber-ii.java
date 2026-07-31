67433a70b455bc14bddd84a5f35d877cdea1bf3a
Tanushri <tanumini456@gmail.com>
2026-07-31 07:32:17 +0000

    Code challenge submission for question: 213 House Robber II

---
+++ 213 House Robber II/213house-robber-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int rob(int[] nums) 
+    {
+        int n=nums.length;
+
+        if(n<2) return nums[0];
+
+        int[]skiplast=new int[n-1];
+
+        int[]skipfirst=new int[n-1];
+
+        for(int i=0;i<n-1;i++)
+        {
+            skiplast[i]=nums[i];
+            skipfirst[i]=nums[i+1];
+        }
+
+        int amount1=robHouse(skiplast);
+        int amount2=robHouse(skipfirst);
+
+        return Math.max(amount1,amount2);
+
+    }
+    private int robHouse(int[]nums)
+    {
+        int n =nums.length;
+        if(n==1) return nums[0];
+
+        int[]dp=new int[n];
+
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+
+        for(int i=2;i<n;i++)
+        {
+            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+        }
+        return dp[n-1];
+    }
+}
