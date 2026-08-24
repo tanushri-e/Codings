380a61eba1cbc7dc7ed62b76a90f5e355f5354f4
Tanushri E <tanumini456@gmail.com>
2026-08-24 18:48:36 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (88.45%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,16 @@
+class Solution 
+{
+    public void reverseString(char[] s) 
+    {
+        int i=0;
+        int j=s.length-1;
+        while(i<j)
+        {
+            char first=s[i];
+            s[i]=s[j];
+            s[j]=first;
+            j--;
+            i++;
+        }
+    }
+}
